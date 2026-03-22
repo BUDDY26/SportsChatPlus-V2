54ae19b182a5596cd042ce9cb5a13884883407cb
Ruben James Aleman <ruben.aleman@gmail.com>
2026-03-22 13:20:19 -0500

    revert: remove auth redirect from (auth) layout  caused infinite loop

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,9 +9,5 @@ export default async function AuthLayout({
 }) {
   const session = await getServerSession(authOptions);
 
-  if (session) {
-    redirect("/dashboard");
-  }
-
   return <>{children}</>;
 }
