cb0a02020da4013a97925e1905ea63b2fbdb2f85
Ruben James Aleman <ruben.aleman@gmail.com>
2026-03-22 16:19:33 -0500

    Revert "fix: wrap dashboard layout children in SessionProvider  resolves useSession crash on ai-insights page"
    This reverts commit 3263a4691a20a28288fd8aa1a1dce2c150dc1578.

--- app/(auth)/dashboard/layout.tsx
+++ app/(auth)/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
-import { SessionProvider } from "next-auth/react";
 import { DashboardNavbar } from "@/components/dashboard/navbar";
 import { DashboardSidebar } from "@/components/dashboard/sidebar";
 
@@ -22,7 +21,7 @@ export default async function DashboardLayout({
       <div className="flex flex-1 min-h-0">
         <DashboardSidebar />
         <main className="flex-1 min-h-0 overflow-y-auto">
-          <SessionProvider>{children}</SessionProvider>
+          {children}
         </main>
       </div>
     </div>
